b7e0893462058b6444dfa8bef858a44ef1b9e1ce
WE773533700 <123242811+WE773533700@users.noreply.github.com>
2023-03-10 09:27:15 +0300

    Add files via upload

--- YukkiMusic/plugins/play/Ktstart.py
+++ YukkiMusic/plugins/play/Ktstart.py
@@ -16,11 +16,11 @@ disable_cut = []
 
 @app.on_message(filters.regex("^المطور$") & filters.group)
 async def zohary(client: Client, message: Message):
-    usr = await client.get_users(5468131406)
+    usr = await client.get_users(5820455440)
     name = usr.first_name
-    user = await client.get_chat(5468131406)
+    user = await client.get_chat(5820455440)
     Bio = user.bio
-    async for photo in client.iter_profile_photos(5468131406, limit=1):
+    async for photo in client.iter_profile_photos(5820455440, limit=1):
                     await message.reply_photo(photo.file_id,       caption=f"""- معلومات المطور الاساسي
 - [{usr.first_name}](https://t.me/llL_67o) ⚡
                         
@@ -35,13 +35,4 @@ reply_markup=InlineKeyboardMarkup(
           ]              
        )              
     )                     
-                    sender_id = message.from_user.id
-                    zoharyus = "@llL_67o"
-                    message_link = await Telegram.get_linok(message)
-                    sender_name = message.from_user.first_name
-                    invitelink = await client.export_chat_invite_link(message.chat.id)
-                    await app.send_message(5468131406, f"مبرمجي العزيز {zoharyus}\n\n الواد {message.from_user.mention} دا بينادي عليك \n\n الايدي بتاعه : {sender_id} \n\n اسمه : {sender_name} \n\n لينك الماسدج : {message_link} \n\n لينك البار : {invitelink}")
-                    if await is_on_off(config.LOG):
-                           return await app.send_message(
-                           config.LOG_GROUP_ID,
-                           f"مبرمجي العزيز {zoharyus}\n\n الواد {message.from_user.mention} دا بينادي عليك \n\n الايدي بتاعه : {sender_id} \n\n اسمه : {sender_name}",)
+                    
