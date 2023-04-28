d11bb7878face3b9e0cd25f690fa2832f2462f0e
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-04-28 22:48:08 +0300

    Add files via upload

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -1,18 +1,27 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import sys
 
 from pyrogram import Client
+from pyrogram.types import BotCommand
 
 import config
 
 from ..logging import LOGGER
 
 
-
-class AnonXBot(Client):
+class YukkiBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "AnonXMusic",
+            "YukkiMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -23,23 +32,42 @@ class AnonXBot(Client):
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
-        if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
-        else:
-            self.name = get_me.first_name
-        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != "administrator":
-            LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
-            )
-            sys.exit()
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, f"**»  تم تشغيل بوتك على 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀  بنجاح :**\n\n✨ ɪᴅ : `{self.id}`\n❄ ɴᴀᴍᴇ : {self.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
+                config.LOG_GROUP_ID, "Bot Started"
             )
         except:
             LOGGER(__name__).error(
                 "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
             sys.exit()
+        if config.SET_CMDS == str(True):
+            try:
+                await self.set_bot_commands(
+                    [
+                        BotCommand("ping", "Check that bot is alive or dead"),
+                        BotCommand("play", "Starts playing the requested song"),
+                        BotCommand("skip", "Moves to the next track in queue"),
+                        BotCommand("pause", "Pause the current playing song"),
+                        BotCommand("resume", "Resume the paused song"),
+                        BotCommand("end", "Clear the queue and leave voice chat"),
+                        BotCommand("shuffle", "Randomly shuffles the queued playlist."),
+                        BotCommand("playmode", "Allows you to change the default playmode for your chat"),
+                        BotCommand("settings", "Open the settings of the music bot for your chat.")
+                        ]
+                    )
+            except:
+                pass
+        else:
+            pass
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Please promote Bot as Admin in Logger Group"
+            )
+            sys.exit()
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
